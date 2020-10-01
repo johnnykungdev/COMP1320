0ccb83f14b78cfecee9bdc827267fd6b4de24be6
Che-Yi Kung <kjohnathan.engi@gmail.com>
2020-10-01 12:09:26 -0700

    follow the airbnb guidline to fix the style of the code

--- lab2/main.js
+++ lab2/main.js
@@ -1,19 +1,19 @@
 const { getDayOfTheWeek, makeCalender } = require("./lab-three.js");
 const readlineSync = require("readline-sync");
 
-function getDayOfTheWeekForUserDate (){
-    var userDate = readlineSync.question("Please enter the the date with format of yyyy-mm-dd. ");
-    const data = userDate.split("-");
-    const year = Number(data[0]);
-    const month = Number(data[1]);
-    const date = Number(data[2]);
-    if ( year && month && date ){
-        const day_in_the_week = getDayOfTheWeek(year, month, date);
-        const date_msg = `${userDate} is ${day_in_the_week}`;
-        console.log(date_msg);
-    } else {
-        console.log("The format of your input is wrong");
-    }
+function getDayOfTheWeekForUserDate() {
+	const userDate = readlineSync.question("Please enter the the date with format of yyyy-mm-dd. ");
+	const data = userDate.split("-");
+	const year = Number(data[0]);
+	const month = Number(data[1]);
+	const date = Number(data[2]);
+	if (year && month && date) {
+			const day_in_the_week = getDayOfTheWeek(year, month, date);
+			const date_msg = `${userDate} is ${day_in_the_week}`;
+			console.log(date_msg);
+	} else {
+			console.log("The format of your input is wrong");
+	}
 };
 
 makeCalender();
